8580e279c7f122b26d03f943c715b6f64531f9e5
unknown <dawidskomorochow@gmail.com>
2020-08-24 18:40:57 +0200

    add header comment article section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ArticleComponent } from './article/article.component';
+import { SectionComponent } from './article/section/section.component';
+import { CommentsComponent } from './article/comments/comments.component';
+import { CommentFormComponent } from './article/comments/comment-form/comment-form.component';
+import { HeaderNComponent } from './header-n/header-n.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ArticleComponent,
+    SectionComponent,
+    CommentsComponent,
+    CommentFormComponent,
+    HeaderNComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header-n/header-n.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-n',
+  templateUrl: './header-n.component.html',
+  styleUrls: ['./header-n.component.css']
+})
+export class HeaderNComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
