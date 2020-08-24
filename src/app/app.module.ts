ac2defa3ac3827ff179dd3c82e6e0aba3559f626
unknown <dawidskomorochow@gmail.com>
2020-08-24 20:37:39 +0200

    add structure folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { ArticleComponent } from './article/article.component';
 import { SectionComponent } from './article/section/section.component';
 import { CommentsComponent } from './article/comments/comments.component';
 import { CommentFormComponent } from './article/comments/comment-form/comment-form.component';
-import { HeaderNComponent } from './header-n/header-n.component';
+import { ContactusComponent } from './home/pages/contactus/contactus.component';
+import { AboutusComponent } from './home/pages/aboutus/aboutus.component';
+import { FooterComponent } from './shared/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+
     ArticleComponent,
     SectionComponent,
     CommentsComponent,
     CommentFormComponent,
-    HeaderNComponent
+    ContactusComponent,
+    AboutusComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
